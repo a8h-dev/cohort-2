184b4a9274be16fa2d0951d8bf1b82a3cbc9aead
Abhishek Mahto <dev.abhishekmahto@gmail.com>
2026-01-14 23:45:25 +0530

    03 JavaScript Basics Done

---
+++ 07-javascript/03-JavaScript-Basics/script.js
@@ -0,0 +1,27 @@
+// strings kya hoti hain
+// slice template-strings(template literal ``) split replace include
+// statements expressions
+
+
+// difference between
+// console.log()
+// console.info()
+// Kuch kuch browsers bas console.info ko thode alag tareke se show krte hain unke aage ek i sign aa jata hai
+
+// https://cutefold.com/
+
+// console.warn()
+// console.err()
+// console.table()
+
+// prompt , alert
+
+// **string**
+// slice
+// `` template string
+// split
+// replace
+// includes
+
+// ek aisa code jo fixed answer dede wo hai statement
+// expression -> jo code koi value deta hai

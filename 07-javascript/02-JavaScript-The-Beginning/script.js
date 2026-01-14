f695955d9992f3805f9e822cbe9eaa764bfbc722
Abhishek Mahto <dev.abhishekmahto@gmail.com>
2026-01-14 22:49:02 +0530

    02 JavaScript The Beginning Done

---
+++ 07-javascript/02-JavaScript-The-Beginning/script.js
@@ -0,0 +1,55 @@
+// Fact:
+// Required...
+// Express, Zero Network, Can't Fix Other's Code, Vision, Leadership Quality
+
+// ----------------------**Introduction to JS**
+// JS ek scripting lauguage hai
+// computers ko binary smajah aati hai jo ki 0 1 mein likhi jati hai
+// 01010100100100010101
+// Abhishek -> translator (Interpreter/Compiler) -> 010100101001101010101
+// JS bhi ek aise language hai jo ham insan likhte hai and  wo 0101010 mein convert ki jati hai taki machines smjh sake
+// Computers are powerful unhe use krne se ham kaam jaldi kar sakte hain
+// issliye unki power ko use krne ke liye we use language like JS
+
+// What is scripting language?
+// A scripting language is a type of programming language that is typically interpreted rather than compiled. These languages are designed to automate tasks, manage data, or control existing software systems rather than building large applications from scratch. 
+
+// Markup language, Programming Language.
+// JS - Brendan Eich
+// Netscape mein kaam krte the
+// 15-16 dino me
+// itni jaldi bani toh kaafi issues reh gaye
+// Jaldbaaji ka kaam ho gya
+// errors and bugs chot gye.
+// to unhe fix krte krte js ke boht se versions aaye
+// unme se ek sabse bada version aaya jiska naam tha ES6
+// uske baad aur bhi version aaye par ES6 sabse bada change raha JS ki history mein, kyuki JS lagbhag completely badal gyi
+// matlab ki JS ke issues and bugs and problems ko fix krne ke liye boht saare naye update diye gye ES6 version me.
+// JS sirf browser pe chalti hai
+// ham index.html se connect krte hain .js file ko
+// jab aapki html file chalegi tab hi aapka js bhi chal jayega
+// ASK GPT -- about all this (try yourself)
+
+// -----------------------------**Why it is important**
+// Kyuki website pe animation -> interaction -> lagbhag har prakaar ka koi engagement create krne ke liye js use hoti hai
+// https://landonorris.com/
+// https://www.anuchome.com/
+
+// ------------------------------**What can it do for you?**
+// alert();
+// let rohan = "Ohhh Shittttt!"
+// console.log(rohan);
+
+// ---------------------------**Script tag use karke attach karna hai js ko**
+// Done Ji
+
+// ---------------------------**JS ko browser mein run karna**
+// Done Ji
+
+// ---------------------**var let const**
+// console.log .warn .err Done Ji
+
+// ---------------------**string kya hoti hai**
+// ---------------------**slice template strings split replace includes**
+
+// TRY CHAT GPT...

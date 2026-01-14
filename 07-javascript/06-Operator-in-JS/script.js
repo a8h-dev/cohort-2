52d050f8756890dc6c28653ef1ec5fd4827ef94e
Abhishek Mahto <dev.abhishekmahto@gmail.com>
2026-01-15 01:41:38 +0530

    06 Operator in JS Done

---
+++ 07-javascript/06-Operator-in-JS/script.js
@@ -0,0 +1,40 @@
+// operators in JS
+
+// 1. Arithmetic Operators
+// + - * / % **
+
+// 2. Assignment Operators
+// = += -= *= /= %=
+
+// 3. Comparison Operators
+// == === != !== > < >= <=
+
+// 4. Logical Operators
+// && || !
+
+// 5. Ternary Operator
+// condition ? doThis : doThat
+
+// 6. Type Checking Operators
+// typeof
+// instanceof ~~ (More Deep Baby)
+
+// 7. String Operator
+// + (for concatenation)
+
+// 8. Spread / Rest Operator
+// ... (very important in modern JS)
+
+// 9. Nullish Coalescing Operator
+// ?? (fallback only when null / undefined)
+
+// 10. Optional Chaining
+// ?. (safe access to nested values)
+
+// 11. variable hoisting in JS
+// - Understanding Condition Operator in JavaScript - [`if`, `else`, `if-else`, `else-if`, `Ternary Operator`, `switch`]
+
+// 12. loop
+// - `for` Loop in JavaScript
+// - `while` Loop in JavaScript
+// - `do...while` in JavaScript

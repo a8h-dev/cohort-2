474382bd89fcabb674ef040c26bd7eee46789ff6
Abhishek Mahto <dev.abhishekmahto@gmail.com>
2026-01-15 01:13:19 +0530

    05 Basic Problems in JS Done

---
+++ 07-javascript/05-Basic-Problems-in-JS/script.js
@@ -0,0 +1,21 @@
+// operators in JS
+
+// 1. Arithmetic Operators
+// + - * / % **
+
+// 2. Assignment Operators
+// = += -= *= /= %=
+
+// 3. Comparison Operators
+// == === != !== > < >= <=
+
+// 4. Logical Operators
+// && || !
+
+// 5. Ternary Operator
+// condition ? doThis : doThat
+
+// 6. Type Checking Operators
+// typeof
+
+// Solve Assignments

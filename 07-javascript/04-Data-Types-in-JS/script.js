5846bac968302c767cb0c69d4c161898053c12a9
Abhishek Mahto <dev.abhishekmahto@gmail.com>
2026-01-15 00:28:05 +0530

    04 Data Types Done

---
+++ 07-javascript/04-Data-Types-in-JS/script.js
@@ -0,0 +1,15 @@
+// add comments
+// float
+// number
+// string
+// boolean
+// null
+// array
+// object
+// Symbol - will learn when we will see object in detail
+// undefined
+// NaN
+// Infinity
+// Relative and primitive datatype in JS
+
+// TRY GPT YOURSELF (DO SOME CRAZY TASKS)

487710184bc975425143b9c31e32c60e9de06061
Abhishek Mahto <dev.abhishekmahto@gmail.com>
2026-01-14 21:33:29 +0530

    01 JavaScript Started Done

--- 07-javascript/01-Starting-JavaScript/script.js
+++ 07-javascript/01-Starting-JavaScript/script.js
@@ -1,1 +1,6 @@
-alert("I turn ideas into code ~a8h.dev");
+// alert("I turn ideas into code ~a8h.dev");
+
+// Why we want to learn JavaScript?
+// Most Imp Part in Web Development.
+
+// Lage rehna hai

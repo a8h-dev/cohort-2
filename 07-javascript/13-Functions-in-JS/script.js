6842fe97acae1c042de9476c37c9f0b11a4bad40
Abhishek Mahto <dev.abhishekmahto@gmail.com>
2026-01-15 23:19:57 +0530

    13 Functions in JS Done

---
+++ 07-javascript/13-Functions-in-JS/script.js
@@ -0,0 +1,21 @@
+// arguments keyword
+
+// Functions in JavaScript
+// - Understanding Function in JavaScript and why its widely used - [parameters, arguments, rest parameters, hoisting, variable hoisting, function hoisting];
+
+// - Parameters in JS - [required, destructured, rest, default]
+
+// - Arguments in JS - [positional, default, spread]
+
+// - Classic Function, Nested Function (function within function), Scope Chain in JS
+
+// Undertanding Immediately Invoked Function Expression (IIFE).
+
+// - More Functions in JS - [Arrow Function, Fat Arrow, Anonymous, Higher Order, Callback, First Class (kamina function - jaha variable waha function), Pure Function, Impure Function]
+
+// - Understanding Scoping in JS - [Global Scope, Function Scope]
+// - Understanding Closures, Scoping 
+
+// https://www.anthem.co.za/
+// https://www.paolovendramini.com/
+// https://grabandgo.pt/

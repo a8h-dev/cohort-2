54bf417692b925842bfe961e7392775e85636ff7
Abhishek Mahto <dev.abhishekmahto@gmail.com>
2026-01-15 09:57:39 +0530

    08 Loops in JS Done

---
+++ 07-javascript/08-Loops-in-JS/script.js
@@ -0,0 +1,26 @@
+// ### Loops and conditionals in JavaScript
+
+
+// Conditionals:
+// - Understanding Condition Operator in JavaScript - [`if`, `else`, `if-else`, `else-if`, `Ternary Operator`, `switch case`]
+
+// Loops:
+// straightforward loop, dynamic loop
+// - `for` Loop in JavaScript
+// - `while` Loop in JavaScript
+// - `do-while` Loop in JavaScript
+// - `forEach` Loop in JavaScript
+// - `for in` Loop in JavaScript
+// - `for of` Loop in JavaScript
+// - Recursion in JavaScript
+// - Loop control statements - [`break`, `continue`]
+
+
+// for loop
+// 5 * 1 = 1 ... 10.
+
+// let tableOf = parseInt(prompt("Enter the table"));
+
+// for(let i = 1; i <= 10; i++){
+//     console.log(`${tableOf} x ${i} = ${tableOf * i}`);
+// }

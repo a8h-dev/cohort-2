4ebadcbc67a6c92c1fc0235c625c367d7698c51b
Abhishek Mahto <dev.abhishekmahto@gmail.com>
2026-01-16 23:11:45 +0530

    15 More on Functions & Arrays Questions Added Solve Myself Later

---
+++ 07-javascript/15-More-on-Functions-&-Arrays/script.js
@@ -0,0 +1,16 @@
+// https://immersive-g.com/ -> Three JS
+// https://gazegames.vercel.app/pong -> pong pong game
+
+
+// **Questions**
+// Write a function sayHello() that prints "Hello JavaScript".
+// Create a function add(a, b) that returns their sum and log the result.
+// Write a functions with a default parameter name = "Guest" that prints "Hi <name>".
+// Use rest parameters to make a function that adds unlimited numbers.
+// Create an IIFE that prints "I run instantly".
+// Make a nested function where the inner one prints a variable from the outer one.
+// Create an array of 5 fruits. Add one at the end and remove one from the beginning.
+// Use a for loop to prints all elements of an array.
+
+// Create an Object person with keys name, age, and city, and print each key's value.
+// Use setTimeout() to "Time's up!" after 2 seconds

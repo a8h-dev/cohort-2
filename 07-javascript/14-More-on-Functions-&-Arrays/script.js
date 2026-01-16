142364a2acd815bb70dc4dfa16298ef828f0a47f
Abhishek Mahto <dev.abhishekmahto@gmail.com>
2026-01-16 22:21:14 +0530

    14 More on Functions & Arrays Done

---
+++ 07-javascript/14-More-on-Functions-&-Arrays/script.js
@@ -0,0 +1,17 @@
+// More on Functions and Arrays
+
+// Understanding Scoping in JS - [Global scope, Function scope]
+// Understanding Closures, Scoping Rule
+
+// **Arrays**
+// - What are Arrays in JS and how to create an array.
+// - Understanding How to Accessing Elements in Array
+// - Functions on Arrays - [push, pop, shift, unshift, indexOf, array destructuring, filter, some, map, reduce, spread operator, slice, reverse, sort, join, toString]
+// - Iterating Over Arrays using - [For loop, forEach]
+
+// **Objects**
+// - Understanding what are objects in JavaScript - [key-value pair]
+// - Creating Objects, Accessing Properties, Deleting Property and Nested Objects
+// - Recognize How Objects Are Stored, Traverse Keys of an Object, Array as Object.
+// - Timing Events - setTimeout(), setInterval(), clearTimeout(), clearInterval()
+// - Operation in Objects - [freeze, seal, destructuring, object methods, this keyword]

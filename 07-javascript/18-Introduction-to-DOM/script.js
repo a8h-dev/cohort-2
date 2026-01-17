26a14edf810c7d066758dadb20beb5da3666789a
Abhishek Mahto <dev.abhishekmahto@gmail.com>
2026-01-17 22:02:10 +0530

    DOM started

---
+++ 07-javascript/18-Introduction-to-DOM/script.js
@@ -0,0 +1,10 @@
+// DOM - Document Object Model
+// Frontend ki JS
+
+// ---learn making css boiler plate---
+
+// Dom 4 Pillers
+// - Selecting of an Element
+// - Changing HTML
+// - Changing CSS
+// - Event Listeners

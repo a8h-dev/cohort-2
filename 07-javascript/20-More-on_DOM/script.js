b849c2499349f1bd417ac88922b0e33673cca4ec
Abhishek Mahto <dev.abhishekmahto@gmail.com>
2026-01-18 17:46:11 +0530

    20 More on DOM lecture completed

---
+++ 07-javascript/20-More-on_DOM/script.js
@@ -0,0 +1,16 @@
+// createElement
+// appending element
+
+
+// What you have to do -
+// make main tag in html, give it height and width 100%
+// make a button and center it. (give it position relative and z-index)
+// in JS select button and main div and add a eventlistener on button
+// inside a function create new element with the help of document.createElement method.
+// if you've successfully created an element print it on console to check it.
+// write innerHTML if you want
+// use .appendChild method to add this new element in div
+
+
+// Task:
+// when use click on button create h1, put random quote in it (from array), give them random position, rotation, scale, color(optional)* and append them to parent.

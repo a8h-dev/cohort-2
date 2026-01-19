f8d0a0284b9b35369c96f082c252a9831cab7e02
Abhishek Mahto <dev.abhishekmahto@gmail.com>
2026-01-19 13:59:17 +0530

    22 lecture completed and Task started

---
+++ 07-javascript/22-More-on-DOM/script.js
@@ -0,0 +1,18 @@
+// 0 LPA vs 3 LPA vs 30 LPA
+
+// 3 LPA - 5 LPA
+// MERN
+
+// 20 LPA - 30 LPA
+// MERN + DSA + DevOps + System Design (Product Architecture) + Connections + Achievements (Aapne kya ukhada hai) + Communication (English) + Personality (Approachable)
+
+// https://wellfound.com/
+// https://cutshort.io/
+
+// https://cubic-bezier.com/
+
+// Videos to watch: TASK: 
+// https://youtu.be/2IPEp_4obGw?si=4VpU4WRvjLcGKyfh
+// https://youtu.be/_7IR39vOKfU?si=lYf2tDnKQAFtgoQA
+// https://youtu.be/_rmSp20X4wo?si=kLjoa2BFFPUCaKZ1
+// JS Projects in YouTube Course by Ankur Bhaiya

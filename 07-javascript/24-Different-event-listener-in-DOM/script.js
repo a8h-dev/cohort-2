cfc9b0f81bf54161fd00f33f08d356794dc50c74
Abhishek Mahto <dev.abhishekmahto@gmail.com>
2026-01-19 17:37:20 +0530

    24 lecture done and piano task started

---
+++ 07-javascript/24-Different-event-listener-in-DOM/script.js
@@ -0,0 +1,12 @@
+// mouseenter
+// mouseleave
+// mousemove
+// keydown
+
+// https://www.musicca.com/piano
+// https://virtualpiano.net/music-sheets/
+
+// TASK:
+// make a keyboard piano with HTML CSS and JS
+// new Audio()
+// .play()

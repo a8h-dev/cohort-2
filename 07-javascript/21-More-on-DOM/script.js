82b343f14ee3466b35d4d444bd2e8cbf54533559
Abhishek Mahto <dev.abhishekmahto@gmail.com>
2026-01-19 12:18:43 +0530

    21 class completed and task started

---
+++ 07-javascript/21-More-on-DOM/script.js
@@ -0,0 +1,12 @@
+// setTimeout -> delay
+// setInterval -> controlled loop
+// clearTimeout
+// clearInterval
+
+// sync -> ek time pe ek process
+// async -> ek time pe multiple process
+
+// JS sync by nature hain and async ham use bana sakte hain
+
+// TASK:
+// Make a downloading effect like this.

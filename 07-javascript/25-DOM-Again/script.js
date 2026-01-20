da1c4bee881814e29336f195ffd272a847d591a4
Abhishek Mahto <dev.abhishekmahto@gmail.com>
2026-01-20 22:15:01 +0530

    25 DOM again Done

---
+++ 07-javascript/25-DOM-Again/script.js
@@ -0,0 +1,9 @@
+// document.querySelectorAll()
+// array jaisa hai but array nhi hai nodelist hai
+// array ke sare features kaam nhi karte kuch kuch kaam karte hain
+
+// CSS clip path
+
+// parent.childNodes
+// text add kr dete hain (break line gap ke wajah se)
+

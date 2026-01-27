87913dd826127e5bec846a46ee03af0a8621bf1c
Abhishek Mahto <dev.abhishekmahto@gmail.com>
2026-01-27 11:46:23 +0530

    Productivity  Dashboard To Do List with reload Done

---
+++ 08-javascript-projects/01-Productivity-Dashboard/script.js
@@ -0,0 +1,75 @@
+function openFeatures() {
+  let allElems = document.querySelectorAll(".elem");
+  let fullElemPage = document.querySelectorAll(".fullElem");
+  let fullElemPageBackBtn = document.querySelectorAll(".fullElem .back");
+
+  allElems.forEach(function (elem) {
+    elem.addEventListener("click", function () {
+      fullElemPage[elem.id].style.display = "block";
+    });
+  });
+
+  fullElemPageBackBtn.forEach(function (back) {
+    back.addEventListener("click", function () {
+      fullElemPage[back.id].style.display = "none";
+    });
+  });
+}
+openFeatures();
+
+function todoList() {
+  let form = document.querySelector(".addTask form");
+  let taskInput = document.querySelector(".addTask form #task-input");
+  let taskDetailsInput = document.querySelector(".addTask form textarea");
+  let taskCheckBox = document.querySelector(".addTask form #check");
+
+  var currentTask = [];
+
+  if (localStorage.getItem("currentTask")) {
+    currentTask = JSON.parse(localStorage.getItem("currentTask"));
+  } else {
+    console.log("Task list is empty");
+  }
+
+  function renderTask() {
+    let allTask = document.querySelector(".allTask");
+    let sum = "";
+
+    currentTask.forEach(function (elem, idx) {
+      sum += `<div class="task">
+        <h5>${elem.task} <span class=${elem.imp}>imp</span></h5>
+        <button id=${idx}>Mark as Completed</button>
+        </div>`;
+    });
+    allTask.innerHTML = sum;
+    localStorage.setItem("currentTask", JSON.stringify(currentTask));
+  }
+  renderTask();
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    currentTask.push({
+      task: taskInput.value,
+      details: taskDetailsInput.value,
+      imp: taskCheckBox.checked,
+    });
+
+    renderTask();
+    location.reload();
+
+    taskInput.value = "";
+    taskDetailsInput.value = "";
+    taskCheckBox.checked = false;
+  });
+
+  let markCompletedBtn = document.querySelectorAll(".task button");
+  markCompletedBtn.forEach(function (btn) {
+    btn.addEventListener("click", function () {
+      currentTask.splice(btn.id, 1);
+      location.reload();
+      renderTask();
+    });
+  });
+}
+todoList();

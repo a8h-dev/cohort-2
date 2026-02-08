a226c0535a4f22a12cb3d7935e2a3dc6bc5f58aa
Abhishek Mahto <dev.abhishekmahto@gmail.com>
2026-02-08 10:29:07 +0530

    Spotify Player Window Added

---
+++ 10-react-js-projects/Mac-OS/src/components/windows/Spotify.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import MacWindow from './MacWindow';
+import './Spotify.scss'
+
+const Spotify = () => {
+  return (
+    <MacWindow>
+        <div className="spotify-window">
+            <iframe data-testid="embed-iframe" style={{borderRadius:"12px"}} src="https://open.spotify.com/embed/playlist/2OK7UqRuCqLTFrHVv0Qdoa?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allowFullScreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+        </div>
+    </MacWindow>
+  );
+}
+
+export default Spotify;

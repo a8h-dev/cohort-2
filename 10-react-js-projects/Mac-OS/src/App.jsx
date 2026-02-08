735c08c055250504cbb4f743e234ca0e4f2d5461
Abhishek Mahto <dev.abhishekmahto@gmail.com>
2026-02-08 10:16:13 +0530

    Resume Window Done

--- 10-react-js-projects/Mac-OS/src/App.jsx
+++ 10-react-js-projects/Mac-OS/src/App.jsx
@@ -3,6 +3,7 @@ import Doc from './components/Doc';
 import Nav from './components/Nav';
 import Github from './components/windows/Github';
 import Note from './components/windows/Note';
+import Resume from './components/windows/Resume';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Doc />
       <Github />
       <Note />
+      <Resume />
     </main>
   );
 }
---
+++ 10-react-js-projects/Mac-OS/src/components/windows/Resume.jsx
@@ -0,0 +1,18 @@
+import MacWindow from "./MacWindow";
+import './Resume.scss';
+
+const Resume = () => {
+  return (
+    <MacWindow>
+      <div className="resume-window">
+        <embed
+          src="https://drive.google.com/file/d/1MCYSA1eRSm4MWwF6L7LPw3lHxF44KbRS/preview"
+        //   width="100%"
+        //   height="600"
+        />
+      </div>
+    </MacWindow>
+  );
+};
+
+export default Resume;
